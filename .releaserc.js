cd6c6299f4f530fb2e506b7155949e6e0eaa2d2e
Tobias Bocanegra <tripod@adobe.com>
2019-08-14 14:16:18 -0700

    chore(ci): use wsk-builder sequence building

--- .releaserc.js
+++ .releaserc.js
@@ -3,17 +3,19 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', {
-      'changelogFile': 'CHANGELOG.md',
+      changelogFile: 'CHANGELOG.md',
     }],
-    ['@semantic-release/exec', {
-      'prepareCmd': 'npm run semantic-prepare ${nextRelease.version}',
-      'publishCmd': 'npm run deploy'
+    ["@semantic-release/npm", {
+      npmPublish: false,
     }],
     ['@semantic-release/git', {
-      'assets': ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
-    ['@semantic-release/github', {}]
+    ['@semantic-release/exec', {
+      publishCmd: 'npm run deploy'
+    }],
+    '@semantic-release/github',
   ],
-  'branch': 'master'
+  branch: 'master'
 };
