435665614f3b48ea85f2a46af6e3a802473ecb46
Tobias Bocanegra <tripod@adobe.com>
2020-12-25 15:01:52 +0900

    chore: fix test

--- test/index.test.js
+++ test/index.test.js
@@ -24,7 +24,7 @@ describe('Index Tests', () => {
   });
 
   it('index function returns an object', async () => {
-    const result = await index();
+    const result = await index({});
     assert.equal(typeof result, 'object');
   });
 });
