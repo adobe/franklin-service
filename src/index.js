bc8d5cf73f3e5d7e9bf3fe49e7befbdf889fa3dc
Tobias Bocanegra <tripod@adobe.com>
2021-05-03 11:16:42 +0900

    fix(deps): use helix shared modules

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const { wrap } = require('@adobe/helix-shared');
+const wrap = require('@adobe/helix-shared-wrap');
 const { logger } = require('@adobe/helix-universal-logger');
 const { wrap: status } = require('@adobe/helix-status');
 const { Response } = require('@adobe/helix-universal');
